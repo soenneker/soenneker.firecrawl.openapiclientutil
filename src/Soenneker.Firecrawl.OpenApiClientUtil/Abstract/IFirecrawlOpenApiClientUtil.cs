304fae2d596954df65a585c383b13fd951fd3783
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:36:49 -0500

    xml commenting

--- src/Soenneker.Firecrawl.OpenApiClientUtil/Abstract/IFirecrawlOpenApiClientUtil.cs
+++ src/Soenneker.Firecrawl.OpenApiClientUtil/Abstract/IFirecrawlOpenApiClientUtil.cs
@@ -10,5 +10,10 @@ namespace Soenneker.Firecrawl.OpenApiClientUtil.Abstract;
 /// </summary>
 public interface IFirecrawlOpenApiClientUtil: IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Gets the value.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task containing the result of the operation.</returns>
     ValueTask<FirecrawlOpenApiClient> Get(CancellationToken cancellationToken = default);
 }
